4040f18783ac7988ad8d0059e0fb1a5b96c00a7d
Andrew Petersen <kirbysayshi@gmail.com>
2013-11-03 23:16:46 -0500

    oops

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 module.exports = function(onhalt, opt_msg, opt_keycode) {
   document.addEventListener('keydown', function(e) {
-    if (e.which == opt_keycode || 27) {
+    if (e.which == (opt_keycode || 27)) {
       onhalt();
       console.log(opt_msg || 'HALT IN THE NAME OF SCIENCE!');
     }
